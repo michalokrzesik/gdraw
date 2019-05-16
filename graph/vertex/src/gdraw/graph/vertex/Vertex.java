dc141560837335fd10ba4b0cac1db29adc7acd92
Michal Okrzesik <michal.okrzesik@comarch.com>
2019-05-16 09:51:10 +0200

    From work

--- graph/vertex/src/gdraw/graph/vertex/Vertex.java
+++ graph/vertex/src/gdraw/graph/vertex/Vertex.java
@@ -2,7 +2,11 @@ package gdraw.graph.vertex;
 
 import gdraw.graph.node.Node;
 import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Vertex {
@@ -14,20 +18,59 @@ public class Vertex {
     private LinkedList<VertexPoint> points;
     private boolean duplex;
     private boolean curved;
+    private boolean selected;
+    private GraphicsContext gc;
+    private double width;
 
-    public Vertex(Node from, Node to, ArrowType arrow, LineType line, Point2D fromPoint, Point2D toPoint, boolean isDuplex, boolean isCurved){
+    public Vertex(Node from, Node to, Point2D fromPoint, Point2D toPoint, GraphicsContext graphicsContext, ArrowType arrow, LineType line, boolean isDuplex, boolean isCurved, double w){
         fromNode = from;
         toNode = to;
+        gc = graphicsContext;
         arrowType = arrow;
         lineType = line;
+        selected = true;
         points = new LinkedList<>();
-        points.addFirst(new VertexPoint(fromPoint));
         points.addLast(new VertexPoint(toPoint));
+        points.addFirst(new VertexPoint(fromPoint));
         duplex = isDuplex;
         curved = isCurved;
+        width = w;
+        draw();
+    }
+
+    private void drawSelect(Point2D point){
+        Paint prev = gc.getFill();
+        gc.setFill(Color.BLUE);
+        gc.fillOval(point.getX() - width/2, point.getY() - width/2, width, width);
+        gc.setFill(prev);
     }
 
 
+    public void draw(){
+        if(curved) drawCurved();
+        else{
+            Iterator<VertexPoint> it = points.iterator();
+            VertexPoint prev = null, now = null;
+            if(it.hasNext())  prev = it.next();
+            lineType.set(gc, width);
+            if(selected) drawSelect(prev.getPoint());
+            while(it.hasNext()){
+                now = it.next();
+                gc.strokeLine(prev.getX(), prev.getY(), now.getX(), now.getY());
+                if(prev.isHardPoint() && now.isHardPoint()){
+                    VertexPoint mid = new VertexPoint(prev.getPoint().midpoint(now.getPoint()), false);
+                    points.add(points.indexOf(now), mid);
+                    if(selected) drawSelect(mid.getPoint());
+                }
+                if(selected)
+                    drawSelect(prev.getPoint());
+            }
+            drawSelect(now.getPoint());
+            arrowType.draw(gc, prev, now);
+            if(duplex) arrowType.draw(gc, points.get(1), points.getFirst());
+        }
+
+    }
 
 
 }
--- graph/vertex/src/gdraw/graph/vertex/VertexPoint.java
+++ graph/vertex/src/gdraw/graph/vertex/VertexPoint.java
@@ -4,16 +4,23 @@ import javafx.geometry.Point2D;
 import org.jetbrains.annotations.NotNull;
 
 public class VertexPoint {
-    Point2D point;
-    VertexPointOrientation orientation;
+    private Point2D point;
+    private VertexPointOrientation orientation;
+    private boolean hardPoint;
 
     public VertexPoint(@NotNull Point2D point){ this(point.getX(), point.getY()); }
+
+    public VertexPoint(@NotNull Point2D point, boolean hardPoint){
+        this(point.getX(), point.getY());
+        this.hardPoint = hardPoint;
+    }
 
     public VertexPoint(double x, double y){ this(x, y, VertexPointOrientation.NONE); }
 
     public VertexPoint(double x, double y, VertexPointOrientation orientation) {
         point = new Point2D(x, y);
         this.orientation = orientation;
+        hardPoint = true;
     }
 
     public double getX(){
@@ -28,7 +35,25 @@ public class VertexPoint {
         point = newPoint;
     }
 
+    public Point2D getPoint() {
+        return point;
+    }
+
     public void setOrientation(VertexPointOrientation newOrientation){
         orientation = newOrientation;
     }
+
+    public VertexPointOrientation getOrientation(){
+        return orientation;
+    }
+
+    public boolean isHardPoint(){
+        return hardPoint;
+    }
+
+    public void setHardPoint(boolean hp){
+        hardPoint = hp;
+    }
+
+
 }
