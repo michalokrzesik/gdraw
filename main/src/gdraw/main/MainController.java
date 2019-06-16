d21cfe77649e653122dd14cd9f93fc5e5d324fad
michal.okrzesik <michal.okrzesik@gmail.com>
2019-06-16 14:11:48 +0200

    GUI changes

--- main/src/gdraw/main/MainController.java
+++ main/src/gdraw/main/MainController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.*;
